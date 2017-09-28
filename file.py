c830271defd60eb1d01c4dd8542d20c5c3a48616
zhsir123 <zhsir123@gmail.com>
2017-09-28 18:29:26 +0900

    rename Checker class
    chekcer class method 추가 및 get_hash함수 추가

--- file.py
+++ file.py
@@ -1,13 +1,36 @@
-import os, time
+import os
+import time
+import hashlib
 
 
-class File:
-    def __init__(self, name, path, hash):
+def get_hash(path):
+    _hash = hashlib.md5()
+    with open(path, 'rb') as file:
+        buf = file.read()
+        _hash.update(buf)
+    return _hash.hexdigest()
+
+
+class Checker:
+    """get file's hash value and check file was changed"""
+    def __init__(self, name, path, _hash):
         self.name = name
         self.path = path
-        self.hash = hash
+        self._hash = _hash
 
     def get_last_modified(self):
-        # get time of recent content modification
-        modified = os.stat(self.path)[8]    
-        return time.ctime(modified)
+        """get time of recent content modification """
+        modified = os.stat(self.path)[8]
+        return time.ctime(modified)        
+
+    def file_changed(self, update):
+        """if file changed return True else return False""" 
+        new_hash = get_hash(self.path)
+        if new_hash is not self._hash:
+            if update:
+                self._hash = new_hash
+            return True
+        return False
+    
+    def is_file_exist(self):
+        return os.path.exists(self.path)
